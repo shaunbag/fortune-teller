24d6c9e8fcfcc4c6b8dcecee4a496c7dc3920c41
Shaunbag <shaunbag@hotmail.com>
2021-03-10 22:42:45 +0000

    Initial commit - javascript functions working, mobile CSS not complete

---
+++ readings.js
@@ -0,0 +1,27 @@
+
+
+const Ace = "Beginnings, Opportunity; Missed Opportunity, Bad Timing"
+const Two = "Balance, Cooperation; Disharmony"
+const Three = "Connection, Sharing; Loss"
+const Four = "Stability, Foundation; Instability"
+const Five = "Activity; Disturbance"
+const Six = "Flow, Pattern, Communication; Ignorance"
+const Seven = "Mystery, Change; Stagnation"
+const Eight = "Movement, Harmony/Health, Time; Imbalance"
+const Nine = "Growth, Wishes; Disappointment, Idealism"
+const Ten = "Completion"
+
+
+const Jacks = "The Jacks represent children and young people. Therefore the Jack can denote something small or the start of something new. In the days before the invention of the telephone, young boys were employed as messengers, so the Jacks can be the messengers for their suits. Jacks can also represent thoughts, so there is the idea of movement connected with these cards. Young person of either sex usually under 35 years old. Can represent a child.  May also represent an older person who is immature, or just young at heart."
+
+const Queens ="The Queens represent women, feminine things,  and the divine feminine principle. Queens represent motherhood and love, and they are nurturing, creative, receptive and introspective. The Queens denote growth and personal power. They bring  emotions and intuition and healing to their respective suits. Traditionally a mature female, but the queen may represent a female of any age. "
+
+const Kings = "The Kings are grown men, and are the highest ranking court card. They denote mastery, authority, control and leadership. The Kings are protective and commanding, but they can be demanding, possessive, and even aggressive and overbearing, depending on their suit and surrounding cards. Male, usually mature, or in a position of authority.  May also represent a younger man who is mature for his age, or is burdened by responsibilities."
+
+const Spades = "Powerful, intense, demanding, controlling, secretive, possessive, charming, mysterious, calculating, detached."
+
+const Hearts = "Emotional, creative, romantic, compassionate, generous, sensitive, nurturing, dependable, protective, moody."
+
+const Diamonds = "Independent, extravagant, impulsive, impatient, optimistic, dynamic, restless, analytical, intelligent, high strung."
+
+const Clubs = "Practical, friendly, reliable, trustworthy, helpful,  ambitious, enthusiastic, sensible, stable, stubborn."
